8eb297a8d279014dd04cfed5c27319ba39790c4a
UemuraMutsuki <251308@st.yoshida-g.ac.jp>
2026-01-30 15:44:08 +0900

    map.cpp.h追加

---
+++ TakoSystem/map.h
@@ -0,0 +1,16 @@
+//=============================================================================
+// 
+// マップヘッダー[map.h]
+// Author : Mutsuki Uemura
+// 
+//=============================================================================
+#ifndef _MAP_H_		
+#define _MAP_H_		// 2重インクルード防止のマクロを定義
+
+// プロトタイプ宣言
+void InitMap(void);
+void UninitMap(void);
+void UpdateMap(void);
+void DrawMap(void);
+
+#endif
