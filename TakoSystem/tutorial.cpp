fd7eb65354609f8696d0b88f7782fe092865f374
Yuna Nakazawa <251322@st.yoshida-g.ac.jp>
2026-02-12 16:48:38 +0900

    チュートリアル画面

--- TakoSystem/tutorial.cpp
+++ TakoSystem/tutorial.cpp
@@ -6,11 +6,33 @@
 //=============================================================================
 #include "main.h"
 #include "tutorial.h"
+#include "camera.h"
 #include "sound.h"
 #include "input.h"
 #include "light.h"
 #include "fade.h"
 #include "model.h"
+#include "meshcylinder.h"
+#include "meshdome.h"
+#include "meshfield.h"
+#include "meshring.h"
+#include "meshorbit.h"
+#include "player.h"
+#include "computer.h"
+#include "pot.h"
+#include "object.h"
+#include "stage.h"
+#include "esa.h"		// エサ
+#include "fishes.h"
+#include "watersurf.h"
+#include "crosshair.h"	// クロスヘア
+#include "ui_gaugeicon.h"
+#include "ui_esa.h"
+#include "oceancurrents.h"
+#include "map.h"
+#include "effect_3d.h"
+#include "particle_3d.h"
+#include "title.h"
 
 // マクロ定義
 #define	MAX_TUTORIAL	(2)	// タイトルで表示するテクスチャの最大数
@@ -26,6 +48,69 @@ void InitTutorial(void)
 	SetLightColor(0, D3DXCOLOR(0.8f, 0.9f, 1.0f, 1.0f));
 	SetLightColor(1, D3DXCOLOR(0.5f, 0.6f, 0.8f, 0.7f));
 	SetLightColor(2, D3DXCOLOR(0.3f, 0.3f, 0.5f, 0.3f));
+
+	// カメラの初期化処理
+	SetNumCamera(GetPlayerSelect());
+	SetCameraPos(0, FIRST_POS, FIRST_POS, CAMERATYPE_PLAYER);
+	SetCameraPos(1, FIRST_POS, FIRST_POS, CAMERATYPE_PLAYER);
+
+	// プレイヤーの初期化処理
+	InitPlayer();
+
+	// CPUの初期化処理
+	InitComputer();
+
+	// メッシュシリンダーの初期化処理
+	InitMeshCylinder();
+	SetMeshCylinder(FIRST_POS, FIRST_POS, D3DXVECTOR2(8.0f, 2.0f), D3DXVECTOR2(INCYLINDER_RADIUS, CYLINDER_HEIGHT), D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), false, MESHCYLINDERTYPE_ROCK);
+
+	// メッシュドームの初期化処理
+	InitMeshDome();
+
+	// メッシュフィールドの初期化処理
+	InitMeshField();
+
+	// メッシュリングの初期化処理
+	InitMeshRing();
+
+	// 3Dエフェクトの初期化処理
+	InitEffect3D();
+
+	// 3Dパーティクルの初期化処理
+	InitParticle3D();
+
+	// 生き物の初期化処理
+	InitFishes();
+
+	// タコつぼの初期化処理
+	InitPot();
+
+	// エサの初期化処理
+	InitEsa();
+
+	// メッシュオービットの初期化処理
+	InitMeshOrbit();
+
+	// 水面の初期化処理
+	InitWaterSurf();
+
+	// 配置物の初期化処理
+	InitObject("data\\objpos001.txt");
+
+	// クロスヘアの初期化処理
+	InitCrossHair();
+
+	// UIゲージアイコンの初期化処理
+	InitUiGaugeIcon();
+
+	// エサUIの初期化処理
+	InitUiEsa();
+
+	// マップの初期化処理
+	InitMap();
+
+	// 海流の初期化処理
+	InitOceanCurrents();
 
 	LPDIRECT3DDEVICE9 pDevice;	// デバイスへのポインタ
 
@@ -105,6 +190,63 @@ void UninitTutorial(void)
 	// サウンドの停止
 	StopSound();
 
+	// プレイヤーの終了処理
+	UninitPlayer();
+
+	// CPUの終了処理
+	UninitComputer();
+
+	// メッシュシリンダーの終了処理
+	UninitMeshCylinder();
+
+	// メッシュドームの終了処理
+	UninitMeshDome();
+
+	// メッシュフィールドの終了処理
+	UninitMeshField();
+
+	// メッシュリングの終了処理
+	UninitMeshRing();
+
+	// 3Dエフェクトの終了処理
+	UninitEffect3D();
+
+	// 3Dパーティクルの終了処理
+	UninitParticle3D();
+
+	// 生き物の終了処理
+	UninitFishes();
+
+	// タコつぼの終了処理
+	UninitPot();
+
+	// エサの終了処理
+	UninitEsa();
+
+	// メッシュオービットの終了処理
+	UninitMeshOrbit();
+
+	// 水面の終了処理
+	UninitWaterSurf();
+
+	// 配置物の終了処理
+	UninitObject();
+
+	// クロスヘアの終了処理
+	UninitCrossHair();
+
+	// UIゲージアイコンの終了処理
+	UninitUiGaugeIcon();
+
+	// エサUIの終了処理
+	UninitUiEsa();
+
+	// マップの終了処理
+	UninitMap();
+
+	// 海流の終了処理
+	UninitOceanCurrents();
+
 	// テクスチャの破棄
 	for (int nCntTutorial = 0; nCntTutorial < MAX_TUTORIAL; nCntTutorial++)
 	{// タイトルの数だけ確認する
@@ -126,6 +268,63 @@ void UninitTutorial(void)
 // リザルトの更新処理
 void UpdateTutorial(void)
 {
+	// プレイヤーの更新処理
+	UpdatePlayer();
+
+	// CPUの更新処理
+	UpdateComputer();
+
+	// メッシュシリンダーの更新処理
+	UpdateMeshCylinder();
+
+	// メッシュドームの更新処理
+	UpdateMeshDome();
+
+	// メッシュフィールドの更新処理
+	UpdateMeshField();
+
+	// メッシュリングの更新処理
+	UpdateMeshRing();
+
+	// 3Dエフェクトの更新処理
+	UpdateEffect3D();
+
+	// 3Dパーティクルの更新処理
+	UpdateParticle3D();
+
+	// 生き物の更新処理
+	UpdateFishes();
+
+	// タコつぼの更新処理
+	UpdatePot();
+
+	// エサの更新処理
+	UpdateEsa();
+
+	// メッシュオービットの更新処理
+	UpdateMeshOrbit();
+
+	// 水面の更新処理
+	UpdateWaterSurf();
+
+	// 配置物の更新処理
+	UpdateObject();
+
+	// クロスヘアの更新処理
+	UpdateCrossHair();
+
+	// UIゲージアイコンの更新処理
+	UpdateUiGaugeIcon();
+
+	// エサUIの更新処理
+	UpdateUiEsa();
+
+	// マップの更新処理
+	UpdateMap();
+
+	// 海流の更新処理
+	UpdateOceanCurrents();
+
 	// フェード情報の取得
 	FADE pFade = GetFade();
 
@@ -141,6 +340,63 @@ void UpdateTutorial(void)
 // リザルトの描画処理
 void DrawTutorial(void)
 {
+	// プレイヤーの描画処理
+	DrawPlayer();
+
+	// CPUの描画処理
+	DrawComputer();
+
+	// メッシュシリンダーの描画処理
+	DrawMeshCylinder();
+
+	// メッシュドームの描画処理
+	DrawMeshDome();
+
+	// メッシュフィールドの描画処理
+	DrawMeshField();
+
+	// メッシュリングの描画処理
+	DrawMeshRing();
+
+	// 3Dエフェクトの描画処理
+	DrawEffect3D();
+
+	// 3Dパーティクルの描画処理
+	DrawParticle3D();
+
+	// 生き物の描画処理
+	DrawFishes();
+
+	// タコつぼの描画処理
+	DrawPot();
+
+	// エサの描画処理
+	DrawEsa();
+
+	// メッシュオービットの描画処理
+	DrawMeshOrbit();
+
+	// 水面の描画処理
+	DrawWaterSurf();
+
+	// 配置物の描画処理
+	DrawObject();
+
+	// クロスヘアの描画処理
+	DrawCrossHair();
+
+	// UIゲージアイコンの描画処理
+	DrawUiGaugeIcon();
+
+	// エサUIの描画処理
+	DrawUiEsa();
+
+	// マップの描画処理
+	DrawMap();
+
+	// 海流の描画処理
+	DrawOceanCurrents();
+
 	LPDIRECT3DDEVICE9 pDevice;	// デバイスへのポインタ
 
 	// デバイスの取得
