233f2a6210aab9dad376362625b624ca6a213fc8
Inoue Yuichi <251306@st.yoshida-g.ac.jp>
2026-02-18 15:03:23 +0900

    ・テキストを一部変更しました。

--- TakoSystem/ranking.cpp
+++ TakoSystem/ranking.cpp
@@ -42,7 +42,7 @@ void InitRanking(void)
 		&g_pTextureRanking[0]);
 
 	D3DXCreateTextureFromFile(pDevice,
-		"data/TEXTURE/RANKING001.png",
+		"data/TEXTURE/RANKING002.png",
 		&g_pTextureRanking[1]);
 
 	D3DXCreateTextureFromFile(pDevice,
