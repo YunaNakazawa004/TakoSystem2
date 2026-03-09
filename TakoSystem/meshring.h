492887ade15d2c8799506b3b2bab8f2e3ed5111a
soki inamura <251305@st.yoshida-g.ac.jp>
2026-03-09 10:36:50 +0900

    エサがポットに向かう時にいないのを修正。衝撃波？のエフェクトを追加しました。

--- TakoSystem/meshring.h
+++ TakoSystem/meshring.h
@@ -10,12 +10,29 @@
 #include "main.h"
 
 //*****************************************************************************
+// 列挙型の定義
+//*****************************************************************************
+
+// メッシュリングの種類
+typedef enum
+{
+	MESHRINGTYPE_NONE = -1,		// [-1]無し
+	MESHRINGTYPE_RIPPLES ,		// [0]波紋
+	MESHRINGTYPE_SHOCKWAVE,		// [1]衝撃波
+
+	MESHRINGTYPE_MAX			// 最大値
+
+}MESHRINGTYPE;
+
+//*****************************************************************************
 // プロトタイプ宣言
 //*****************************************************************************
 void InitMeshRing(void);
 void UninitMeshRing(void);
 void UpdateMeshRing(void);
 void DrawMeshRing(void);
-void SetMeshRing(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR2 block, D3DXVECTOR2 size, D3DXCOLOR col);
+void SetMeshRing(MESHRINGTYPE type, D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR2 block, D3DXVECTOR2 size, D3DXCOLOR col);
+
+D3DXVECTOR3 CalcShockWaveRot(D3DXVECTOR3 pos1, D3DXVECTOR3 pos2);	// 衝撃波の角度を求める
 
 #endif
